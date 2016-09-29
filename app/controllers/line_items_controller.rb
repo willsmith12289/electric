4936c383968054aa16ae05715de55a5ff29c253d
will <willsmith12289@gmail.com>
2016-09-28 21:49:32 -0400

    figured out

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,7 +32,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to root }
+        format.html { redirect_to @line_item.cart }
         format.js
         format.json { render :show, status: :created, location: @line_item }
       else
