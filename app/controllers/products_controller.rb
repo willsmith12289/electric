7200c2e930a9fa8788a60b3af0902482e7bbf2a9
will <wsmi1289@gmail.com>
2018-05-20 16:26:25 -0400

    Little nokogiri web scraping practice

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   before_action :set_cart
+  require 'open-uri'
   # GET /products
   # GET /products.json
   def index
@@ -62,10 +63,65 @@ class ProductsController < ApplicationController
   end
 
   def search
-     @products = Product.search(params[:q]).order("created_at DESC")
-     render "index"
+    
+    doc = Nokogiri::HTML(open("https://www.homedepot.com/s/'#{params[:q].downcase.split.join('-')}'?NCNI-5")) do |config|
+      config.strict.noblanks
+    end
+    itemImg = doc.css('div.js-pod img')
+    itemTitle = doc.css('div.plp-pod__info .pod-plp__description')
+    itemDescription = doc.css('div.plp-pod__info .pod-plp__description a')
+    itemPrice = doc.css('div.plp-pod__info .price__wrapper .price')
+    descripts = Array.new()
+    itemDescription.each{|id| descripts<<id['href']}
+    imgs = Array.new()
+    itemImg.each{|i| imgs<<i['src'].strip}
+    titles = Array.new
+    itemTitle.each{|t| titles<<title = t.content.strip.split(' ').drop(1).join(' ').to_s}
+    prices = Array.new()
+    itemPrice.each do |p| 
+      prices<<price = (p.content.include?('/')) ? p.content.split('/')[0].strip : p.content.strip
+    end
+    puts prices
+    itemTitle.count.times do |x|
+      products = {title: titles[x], price: prices[x], description: descripts[x], image_url: imgs[x]}
+      if Product.create(products).valid?
+        Product.create(products)
+      else
+        next
+      end
+    end
+    # imgdesc = imgs.zip(descripts)
+    # products = titlePrice.zip(imgdesc)
+    # @products = products.each do |p|
+    #   p.to_a
+    #   prod = Product.new(p['title'], p.image_url, p.description, p.price)
+    #   return prod
+    # end
+    # itemImg.each_with_index do |product, index|
+    #   title = itemTitle[index](p.content.strip.start_with? '$') ?
+    #   img = itemImg[index]
+    #   description = itemDescription[index]
+    #   price = itemPrice[index]
+    #   # Product.new(,)
+    #   puts description
+    # end
+      # Product.new()
+      # img.each{|i| puts i['src']}
+      # title.each{|t| puts t.content}
+      # price.each{|p| puts p.content}
+
+    # clean_up(itemImg, itemTitle, itemPrice)
+    @products = Product.search(params[:q]).order("created_at DESC")
+    # @products = Product.all
+    render "index"
   end
   private
+
+    def clean_up(img, title, price)
+      img.count.times do |product|
+
+      end
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,7 @@ class Product < ActiveRecord::Base
   #before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :title, :description, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  # validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
