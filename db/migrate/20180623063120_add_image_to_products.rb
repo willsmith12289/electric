e8090edab25ec17954306ab38103bf4e926bf20c
will <wsmi1289@gmail.com>
2018-06-23 03:43:28 -0400

    rm migrations

--- db/migrate/20180623063120_add_image_to_products.rb
+++
@@ -1,6 +0,0 @@
-class AddImageToProducts < ActiveRecord::Migration
-  def change
-    remove_column :products, :image, :string
-    add_column :products, :image, :json
-  end
-end
--- db/migrate/20180623065734_update_image_on_products.rb
+++
@@ -1,6 +0,0 @@
-class UpdateImageOnProducts < ActiveRecord::Migration
-  def change
-    remove_column :products, :image, :json
-    add_column :products, :image, :string
-  end
-end
